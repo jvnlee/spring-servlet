05906b70d437284251b00429704f0ba05a560a51
jvnlee <lhjun1028@naver.com>
2022-03-13 22:21:17 +0900

    Initial setup with spring initializr

---
+++ src/test/java/servlet/springservlet/SpringServletApplicationTests.java
@@ -0,0 +1,13 @@
+package servlet.springservlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
