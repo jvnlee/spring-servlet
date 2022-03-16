e9c2d0c73364d743f5e5fcda1a33c2c25e0ea493
jvnlee <lhjun1028@naver.com>
2022-03-16 23:07:11 +0900

    [ADD] HTTP 응답 보내기 - HTML 또는 JSON

---
+++ src/main/java/servlet/springservlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,33 @@
+package servlet.springservlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import servlet.springservlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Content-Type: application/json;charset=utf-8
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        // Java 객체를 ObjectMapper로 JSON 형태로 만든 뒤 전송
+        HelloData helloData = new HelloData();
+        helloData.setUsername("andy");
+        helloData.setAge(20);
+
+        String result = objectMapper.writeValueAsString(helloData);
+        response.getWriter().write(result);
+    }
+
+}
